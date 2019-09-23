5642193501573f787deec9a9e71c4d340e111b43
sandip <sandip.mavani@corp.avinashi.com>
2019-09-23 18:04:05 +0530

    generate id from hardisk serial processor serial

--- id.go
+++ id.go
@@ -20,14 +20,27 @@
 package hardwareid // import "github.com/sandipmavani/hardwareid"
 
 import (
+	"crypto/md5"
+	"errors"
 	"fmt"
 	"net"
+	"os/exec"
+	"runtime"
+	"strings"
 )
 
 // ID returns the platform specific hardware id of the current host OS.
 // Regard the returned id as "confidential" and consider using ProtectedID() instead.
 func ID() (string, error) {
 	id, err := hardwareId()
+	if err != nil {
+		return "", fmt.Errorf("hardwareid: %v", err)
+	}
+	return id, nil
+}
+
+func physicalId() (string, error) {
+	id, err := GetPhysicalId()
 	if err != nil {
 		return "", fmt.Errorf("hardwareid: %v", err)
 	}
@@ -59,4 +72,108 @@ func hardwareId() (string, error) {
 
 	return "", nil
 
+}
+func Md5(data []byte) string {
+	has := md5.Sum(data)
+	md5Str := fmt.Sprintf("%X", has)
+	return md5Str
+}
+func GetPhysicalId() (string, error) {
+	biosSn, err := GetBIOSSerialNumber()
+	if err != nil {
+		return "", err
+	}
+	diskSn, err := GetDiskDriverSerialNumber()
+	if err != nil {
+		return "", err
+	}
+	cpuId, err := GetCPUPorcessorID()
+	if err != nil {
+		return "", err
+	}
+	return strings.ToUpper(Md5([]byte(biosSn + diskSn + cpuId))), nil
+}
+
+//获取硬盘SerialNumber
+func GetDiskDriverSerialNumber() (string, error) {
+	switch runtime.GOOS {
+	case "windows":
+		return diskDriverSerialNumberOnWindows()
+	case "linux":
+		return "", errors.New("unsupported os")
+	case "darwin":
+		return "", errors.New("unsupported os")
+	default:
+		return "", errors.New("unknown os")
+	}
+}
+
+func diskDriverSerialNumberOnWindows() (string, error) {
+	cmd := exec.Command("CMD", "/C", "WMIC DISKDRIVE GET SERIALNUMBER")
+	serialNo, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	l := strings.Split(string(serialNo), "\n")
+	if len(l) >= 2 {
+		return l[1], nil
+	} else {
+		return "", errors.New("return split length less 2")
+	}
+}
+
+//获取硬盘SerialNumber
+func GetBIOSSerialNumber() (string, error) {
+	switch runtime.GOOS {
+	case "windows":
+		return biosSerialNumberOnWindows()
+	case "linux":
+		return "", errors.New("unsupported os")
+	case "darwin":
+		return "", errors.New("unsupported os")
+	default:
+		return "", errors.New("unknown os")
+	}
+}
+
+func biosSerialNumberOnWindows() (string, error) {
+	cmd := exec.Command("CMD", "/C", "WMIC BIOS GET SERIALNUMBER")
+	serialNo, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	l := strings.Split(string(serialNo), "\n")
+	if len(l) >= 2 {
+		return l[1], nil
+	} else {
+		return "", errors.New("return split length less 2")
+	}
+}
+
+//获取CPU PorcessorID
+func GetCPUPorcessorID() (string, error) {
+	switch runtime.GOOS {
+	case "windows":
+		return cpuPorcessorIDOnWindows()
+	case "linux":
+		return "", errors.New("unsupported os")
+	case "darwin":
+		return "", errors.New("unsupported os")
+	default:
+		return "", errors.New("unknown os")
+	}
+}
+
+func cpuPorcessorIDOnWindows() (string, error) {
+	cmd := exec.Command("CMD", "/C", "WMIC CPU GET ProcessorID")
+	serialNo, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	l := strings.Split(string(serialNo), "\n")
+	if len(l) >= 2 {
+		return l[1], nil
+	} else {
+		return "", errors.New("return split length less 2")
+	}
 }
